13911b7c16bb9813d49f2420c4b49fb97877255e
drewphillips <drew@superheroprinting.com>
2018-07-07 16:11:04 -0700

    3 working API's

--- liri.js
+++ liri.js
@@ -46,8 +46,88 @@ function runSpotify(search){
         search = "I want it that way";
     }
 
+
+
     console.log("run spotify api")
     console.log("search this artist: " + search);
+
+    spotify.search({ type: 'track', query: search }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+     console.log(JSON.stringify(data, null, 2));
+
+    // Album
+    // Artist 
+    // Song name
+    // preview link from Spotify
+
+      var spotifyArray = data.tracks.items;
+
+    //   console.log(spotifyArray[0]);
+
+      console.log("Artist: ", spotifyArray[0].album.artists[0].name);
+      console.log("Song: " +  spotifyArray[0].name)
+      console.log("Preview URL: " + spotifyArray[0].album.artists[0].external_urls.spotify )
+      console.log("Album: ", spotifyArray[0].album.name);
+
+        // for(var i = 0; i < spotifyArray.length; i++){
+        //     console.log(spotifyArray[i]);
+        // }
+      });   // copy here
+}
+
+function runMovie(movie){
+
+    if(!movie){
+        movie = "Mr. Nobody";
+    }
+
+    // Movie Title
+    // Year
+    // Rating - IMBD
+    // Rotten Tomatoes
+    // Country
+    // Plot
+    // Actors
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function(error, response, body){
+        if(!error && response.statusCode === 200){
+            // console.log(body);
+            var movieData = JSON.parse(body);
+            // console.log(movieData);
+
+            console.log("Title: " + movieData.Title)
+            console.log("Year: " + movieData.Year)
+            console.log("IMBD Rating: " + movieData.Rated)
+            console.log("Rotten Tomatoes: " + movieData.Ratings[1].Value)
+            console.log("Country: " + movieData.Country)
+            console.log("Plot: " + movieData.Plot)
+            console.log("Actors: " + movieData.Actors)
+        }
+    })
+
+
+
+}
+
+function runDoWhat(){
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if(error){
+            return console.log(error);
+        }
+
+        // console.log(data);
+
+        var commandArray = data.split(",");
+
+        // console.log(commandArray);
+
+        runApi(commandArray[0], commandArray[1]);
+    })
 }
 
 // getTweets("piratesaz");
@@ -65,6 +145,14 @@ function runApi(command, search){
 
         case "spotify-this":
         runSpotify(search);
+        break;
+
+        case "movie-this":
+        runMovie(search);
+        break;
+
+        case "do-what-it-says":
+        runDoWhat();
         break;
 
         default:
